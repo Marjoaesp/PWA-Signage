9dbae3700925763ab4d3fd88525b35f146258a32
Martin Parisi <martin.parisi@desarrollosnea.com>
2024-05-14 12:09:02 -0300

    removing full screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,8 @@
 // App.tsx
 import React from 'react';
 import Slideshow from './SlideShow'; // Make sure the path is correct
-import { useEffect } from 'react';
 const App: React.FC = () => {
-    useEffect(() => {
-        const appElement = document.documentElement; // Elemento raíz de la aplicación
     
-        function enableFullScreen() {
-          if (appElement.requestFullscreen) {
-            appElement.requestFullscreen();
-          } else if (appElement.requestFullscreen) {
-            appElement.requestFullscreen();
-          } else if (appElement.requestFullscreen) {
-            appElement.requestFullscreen();
-          }
-        }
-    
-        // Llamada a la función para activar el modo pantalla completa
-        enableFullScreen();
-    
-        // Escuchar el evento resize para ajustar el contenido cuando cambie el tamaño de la ventana
-        window.addEventListener('resize', enableFullScreen);
-    
-        return () => {
-          // Eliminar el evento de escucha cuando el componente se desmonte
-          window.removeEventListener('resize', enableFullScreen);
-        };
-      }, []);
     
     return (
         <div className="App">
